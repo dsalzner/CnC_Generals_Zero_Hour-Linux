3db63a478e3cf2d5d088a284efa8ba01ca11b899
Dennis Salzner <mail@dennissalzner.de>
2025-03-06 23:47:34 +0100

    add dependencies for cnc

--- dependencies.sh
+++ dependencies.sh
@@ -5,6 +5,7 @@ cd lib/
 wget https://github.com/edgeforce/directx8/archive/e6fa10f5f92bf523c15fdb068206ce25c8d2121d.zip
 wget https://github.com/microsoft/DirectXMath/archive/126bcb541afd4af2f95c419143682c183037e799.zip
 wget https://github.com/microsoft/DXUT/archive/438cba4545b5f3f3db0487249eb2dbc22b24e7db.zip
+wget https://github.com/ryandrake08/lzhl/archive/ba3ac10ac76c3cf9a875ef477f45563865e2161a.zip
 
 for file in *.zip
 do
@@ -14,6 +15,14 @@ done
 mv "directx8-e6fa10f5f92bf523c15fdb068206ce25c8d2121d" directx8
 mv "DirectXMath-126bcb541afd4af2f95c419143682c183037e799" directx-math
 mv "DXUT-438cba4545b5f3f3db0487249eb2dbc22b24e7db" directx-dxut
+mv "lzhl-ba3ac10ac76c3cf9a875ef477f45563865e2161a" lzhl
+
+wget "https://osdn.net/frs/g_redir.php?m=jaist&f=stlport%2FSTLport+archive%2FSTLport+4%2FSTLport-4.5.3.tar.gz" -O stlport-4.5.3.tar.gz
+tar -xvf stlport-4.5.3.tar.gz
+mv "STLport-4.5.3" stlport
+
+wget https://sourceforge.net/projects/gnuwin32/files/zlib/1.1.4/zlib-1.1.4-src.zip/download -O zlib-1.1.4.zip
+unzip zlib-1.1.4.zip -d zlib-1.1.4
 
 sed -i 's/\#include <Windows.h>/\/\/#include <windows.h>/g' directx-dxut/Core/DXUT.h
 sed -i 's/\#include <Windows.h>/\/\/#include <windows.h>/g' directx-dxut/Core/dxerr.h
